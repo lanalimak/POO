67f82f04677c23d8ae2c8d16a764224050ae2345
Allana <allananaua17@gmail.com>
2025-05-08 09:43:03 -0300

    comandos (refazer depois)

--- biblioteca.py
+++ biblioteca.py
@@ -1,20 +1,21 @@
 class Pessoa():
-    def _init_(self, nome, idade, peso):
+    def __init__(self, nome, idade, peso):
         self.nome = nome
         self.idade = idade
         self.peso = peso
         self.comendo=False
         self.dormindo=False
         self.falando=False
-    def falar(self):
-        if self.falando == True:
-            print("nao pode falar pois ja esta falando")
-        print(f" {self.nome} começou a falar ")
+    def dormir(self):
+        if self.dormindo == True:
+            print(f" {self.nome} está dormindo.")
+        elif
+        print(f" {self.nome} não pode dormir porque está falando")
     def comer(self, comida):
         if self.comendo == True:
-            print("nao pode comer pois ja esta comendo")
+            print("não pode comer, pois, já está comendo")
         print(f" foi comer {comida}")
     def dormir(self):
         if self.dormindo == True:
-            print("nao pode dormir pois ja esta dormindo")
+            print(f" {self.nome} já está dormindo.")
         print(f" {self.nome} começou a dormir")
